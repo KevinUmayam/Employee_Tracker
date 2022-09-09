6ef2f8cb5f37cbbd3bb8a0f6d98f39a7624a3a39
Kevin U <owner@Kevins-MacBook-Pro.local>
2022-09-08 18:22:54 -0700

    fixes to tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,22 +9,33 @@ VALUES
 ("accounting"),
 ("finance"),
 ("sales"),
-("marketing");
+("marketing"),
+("company");
 
 INSERT INTO roles(role_title, 
                   role_salary, 
                   department_id)
-VALUES("employee", 60000,1),
-       ("employee", 50000,2),
-       ("employee", 70000,3),
-       ("employee", 75000,3);
-
+VALUES("sales associate", 60000,3),
+       ("finance agent", 70000,2),
+       ("accountant", 70000,1),
+       ("marketing agent", 75000,4);
+       ("accounting manager", 100000,1);
+       ("finance manager", 100000,2);
+       ("sales manager", 100000,3);
+       ("marketing manager", 100000,4);
+         ("CEO", 1000000,5)
 INSERT INTO employees(first_name, 
                       last_name, 
                       roles_id, 
                       manager_id)
-VALUES ("kevin","umayam",1, null),
-       ("dru","sanchez",2, 1),
-       ("michell","lopez",3,null),
-        ("brayan","meza",4,null);
+VALUES ("kevin","umayam",1, 7),
+       ("dru","sanchez",2, 6),
+       ("michell","lopez",3, 5),
+        ("brayan","meza",4, 8);
+        ("allison","hugs",5, 9);
+        ("ryan","reynolds",6, 9);
+        ("harry","potter",7, 9);
+        ("barack","obama",8, 9);
+           ("darth","vader",8,null)
+
 
